ca1aa6f716bfffacb728c8d91259f530bb4d5282
Justin <efejustin3@gmail.com>
2021-02-03 21:28:21 +0100

    first commit

---
+++ src/robots.js
@@ -0,0 +1,49 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'mental case',
+        username: 'st kolo',
+        email: 'welcom3@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Author Mensah',
+        username: 'Rv Father',
+        email: 'williesohwo@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Mc curtis',
+        username: 'macutur',
+        email: 'wwwwom3@g.com'
+    },
+    {
+        id: 4,
+        name: 'Emmanuel Duru',
+        username: 'No giving up',
+        email: 'TrustingGodForHelp@.com'
+    },  {
+        id: 5,
+        name: 'Make money',
+        username: 'practise',
+        email: 'welcom3@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Erabor Faith',
+        username: 'Rv Father',
+        email: 'willimo@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Arthur curtis',
+        username: 'Yes oocutur',
+        email: 'wwwwom3@g.com'
+    },
+    {
+        id: 8,
+        name: 'Anadu Ozioma',
+        username: 'No giving up',
+        email: 'trustingbaba@.com'
+    }
+]
---
+++ writeup.js
@@ -0,0 +1,8 @@
+// 
+// const stringifiedTest = JSON.stringify(MyJsonFile);
+const arr = [{name: 'efe'}, {name: 'justin'}];
+// const filteredRobots = arr.filter(myname => {
+//         return myname.name.includes('');
+//     })
+
+    // console.log(Boolean(''));
